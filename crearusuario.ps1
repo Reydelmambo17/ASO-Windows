2e85f944855c8f0e3d4dbff88c129ba2fd1bc368
Pablo Arques Manjón <reydelmambo17@gmail.com>
2020-11-11 13:24:58 +0100

    fixing tabs

--- crearusuario.ps1
+++ crearusuario.ps1
@@ -1,8 +1,8 @@
 ﻿$usuario=Read-Host "Introduce nombre de usuario"
 $null=Get-LocalUser -Name "$usuario" 2> $null
 if ($? -eq $true) {
-    Write-Host "El usuario ya existe"
-    exit
+	Write-Host "El usuario ya existe"
+	exit
 }
 $contraseña=Read-Host "Introduce contraseña" -AsSecureString
 $null=New-LocalUser $usuario -Password $contraseña
