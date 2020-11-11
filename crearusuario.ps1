b4e05ce46e762725cd75ce0fa37c12f649da700e
Pablo Arques Manjón <reydelmambo17@gmail.com>
2020-11-11 12:55:55 +0100

    first commit

---
+++ crearusuario.ps1
@@ -0,0 +1,9 @@
+﻿$usuario=Read-Host "Introduce nombre de usuario"
+$null=Get-LocalUser -Name "$usuario" 2> $null
+if ($? -eq $true) {
+    Write-Host "El usuario ya existe"
+    exit
+}
+$contraseña=Read-Host "Introduce contraseña" -AsSecureString
+$null=New-LocalUser $usuario -Password $contraseña
+Add-LocalGroupMember usuarios -Member $usuario
